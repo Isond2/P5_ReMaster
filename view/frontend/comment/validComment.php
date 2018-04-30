6d5adcfd19195ee211e329e3864d77b23a8abea7
Isond2 <30302484+Isond2@users.noreply.github.com>
2018-04-30 14:56:35 +0200

    XSS

--- view/frontend/comment/validComment.php
+++ view/frontend/comment/validComment.php
@@ -16,13 +16,13 @@
 
 <div id="contenupage" class="center">
     <?php if (isset($_SESSION['id']) and isset($_SESSION['nickname']) and $_SESSION['role']==true) {?>
-    <p><?php echo esc_attr($message); ?></p>
+    <p><?php echo htmlspecialchars($message); ?></p>
     <a href="index.php?action=listPosts" class="btn btn-default"> Revenir à la liste des arcticles </a>
     <br />
     <br />
     <form method="post">
             <input type="hidden" name="csrf" value="csrf">
-            <input type="submit" value="<?php echo esc_attr($btn); ?>" class="btn btn-warning" />
+            <input type="submit" value="<?php echo htmlspecialchars($btn); ?>" class="btn btn-warning" />
     </form>
     <?php } else { ?>
     <p> Vous ne disposez pas des droit nécéssaires pour l'administration des commentaires </p>
